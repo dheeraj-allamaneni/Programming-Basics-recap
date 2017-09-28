6dafde4b219e039a401da310567aef72b7eff870
Dheeraj allamaneni <a.dheeraj.95@gmail.com>
2017-09-28 20:40:03 +0530

    Update Modular_Exponentiation.c

--- Modular_Exponentiation.c
+++ Modular_Exponentiation.c
@@ -1,3 +1,20 @@
+/*
+
+Modular Exponentiation (Power in Modular Arithmetic)
+
+Given three numbers x, n and m, compute (x^n) % m.
+
+Examples
+
+Input:  x = 2, y = 3, p = 5
+Output: 3
+Explanation: 2^3 % 5 = 8 % 5 = 3.
+
+Input:  x = 2, y = 5, p = 13
+Output: 6
+Explanation: 2^5 % 13 = 32 % 13 = 6.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
